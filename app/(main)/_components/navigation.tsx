37bf9e5d3bbc1f8b0ec140a79eb048ae5de4c188
Stephanie <stephaniiehallberg@gmail.com>
2023-11-23 15:50:38 +0100

    add: fix for z-index on sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -99,7 +99,7 @@ export const Navigation = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0",
         )}
